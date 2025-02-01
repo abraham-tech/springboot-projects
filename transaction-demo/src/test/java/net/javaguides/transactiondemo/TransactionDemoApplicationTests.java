19758810e311bb6a70e49f836162e3ff467c84e5
abraham <abrahammathewos.meja.5695@student.uu.se>
2025-02-01 14:09:18 -0600

    Add transacton demo project

---
+++ transaction-demo/src/test/java/net/javaguides/transactiondemo/TransactionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.transactiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
