591f8b0fb4c6545e2a6a77734d998de39f6c722b
abraham <abrahammathewos.meja.5695@student.uu.se>
2025-02-02 22:14:16 -0600

    Update product controller

---
+++ search-api/src/main/java/net/javaguides/searchapi/controller/ProductController.java
@@ -0,0 +1,30 @@
+package net.javaguides.searchapi.controller;
+
+import net.javaguides.searchapi.entity.Product;
+import net.javaguides.searchapi.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Product>> searchProducts(@RequestParam("query") String query){
+        return ResponseEntity.ok(productService.searchProducts(query));
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product){
+        return productService.createProduct(product);
+    }
+}
+
