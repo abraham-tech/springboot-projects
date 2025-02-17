c6f73484e694255b7afe5b438b8387f83ffebd3b
abraham <abrahammathewos.meja.5695@student.uu.se>
2025-02-16 23:08:32 -0600

    Java reactive programming

---
+++ webFlux-reactive-crud/src/main/java/net/javaguides/webfluxreactivecrud/WebFluxReactiveCrudApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.webfluxreactivecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxReactiveCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxReactiveCrudApplication.class, args);
+    }
+
+}
