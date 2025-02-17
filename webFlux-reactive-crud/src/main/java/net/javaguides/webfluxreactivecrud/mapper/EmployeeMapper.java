02bd5f404a4d3d18517355ab98cbd86531c50bec
abraham <abrahammathewos.meja.5695@student.uu.se>
2025-02-16 23:39:53 -0600

    Create employee mapper class

---
+++ webFlux-reactive-crud/src/main/java/net/javaguides/webfluxreactivecrud/mapper/EmployeeMapper.java
@@ -0,0 +1,26 @@
+package net.javaguides.webfluxreactivecrud.mapper;
+
+import net.javaguides.webfluxreactivecrud.dto.EmployeeDto;
+import net.javaguides.webfluxreactivecrud.entity.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
+
