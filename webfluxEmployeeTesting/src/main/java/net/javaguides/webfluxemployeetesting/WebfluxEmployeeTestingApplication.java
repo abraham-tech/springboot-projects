17c1f3b47909eb8f47552c00dc31038a866e4676
abraham <abrahammathewos.meja.5695@student.uu.se>
2025-03-04 22:16:54 -0600

    Create a new project

---
+++ webfluxEmployeeTesting/src/main/java/net/javaguides/webfluxemployeetesting/WebfluxEmployeeTestingApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.webfluxemployeetesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxEmployeeTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxEmployeeTestingApplication.class, args);
+    }
+
+}
